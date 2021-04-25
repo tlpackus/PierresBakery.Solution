d7593a1b4683dfb301f8805e25df613da3e2d63a
tlpackus <tlpackus@gmail.com>
2021-04-24 20:38:18 -0700

    add boilerplate code to test the 2 classes

---
+++ BakeryTests/ModelTests/BreadOrderTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BreadOrderTests
+  {
+    // [TestMethod]
+    // public void BreadOrderConstructor_CreatesInstanceOfBreadOrder_BreadOrder()
+    // {
+    //   BreadOrder newBreadOrder = new BreadOrder();
+    //   Assert.AreEqual(typeof(BreadOrder), newBreadOrder.GetType());
+    // }
+  }
+} 
---
+++ BakeryTests/ModelTests/PastryOrderTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class PastryOrderTests
+  {
+    // [TestMethod]
+    // public void PastryOrderConstructor_CreatesInstanceOfPastryOrder_PastryOrder()
+    // {
+    //   PastryOrder newPastryOrder = new PastryOrder();
+    //   Assert.AreEqual(typeof(PastryOrder), newPastryOrder.GetType());
+    // }
+  }
+} 
