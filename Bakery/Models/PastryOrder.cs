323b5eb9cf6dff548258f6a531e9dd13d8306dde
tlpackus <tlpackus@gmail.com>
2021-04-25 22:48:04 -0700

    add good fail for PastryOrder constructor

--- Bakery/Models/PastryOrder.cs
+++ Bakery/Models/PastryOrder.cs
@@ -2,6 +2,11 @@ namespace Bakery.Models
 {
   public class PastryOrder
   {
-    
+    public int OrderedPastry { get; set; }
+
+    public PastryOrder(int pastryOrder)
+    {
+
+    }
   }
 } 
--- BakeryTests/ModelTests/PastryOrderTests.cs
+++ BakeryTests/ModelTests/PastryOrderTests.cs
@@ -9,8 +9,16 @@ namespace Bakery.Tests
     [TestMethod]
     public void PastryOrderConstructor_CreatesInstanceOfPastryOrder_PastryOrder()
     {
-      PastryOrder newPastryOrder = new PastryOrder();
+      PastryOrder newPastryOrder = new PastryOrder(1);
       Assert.AreEqual(typeof(PastryOrder), newPastryOrder.GetType());
     }
-  }
-} 
+    [TestMethod]
+    public void GetPastryOrder_ReturnsPastriesOrdered_Int()
+    {
+      int orderedPastry = 1;
+      PastryOrder newPastryOrder = new PastryOrder(1);
+      int result = newPastryOrder.OrderedPastry;
+      Assert.AreEqual(orderedPastry, result);
+    }
+  } 
+}
