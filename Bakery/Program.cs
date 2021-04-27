2648bcced867c9e905a37db1bef0ccf022ceca64
tlpackus <tlpackus@gmail.com>
2021-04-26 17:29:42 -0700

    add logic to introduce the bakery and their deals in the console

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,10 +1,17 @@
+using System;
+
 namespace Dealership
 {
   public class Program
   {
     public static void Main()
     {
-      
+      Console.WriteLine("Welcome To Pierre's Bakery!");
+      Console.WriteLine("Our world renowned Bread and Pastry's are on sale today!");
+      Console.WriteLine("BREAD is $5 a loaf.");
+      Console.WriteLine("DEAl: Buy 2 get 1 free!");
+      Console.WriteLine("PASTRY'S are $2 each.");
+      Console.WriteLine("DEAL: 3 for $5!");
     }
   }
 }
