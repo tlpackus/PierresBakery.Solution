ec62088e19f081ac4f7206b50d47dc190921838c
tlpackus <tlpackus@gmail.com>
2021-04-26 17:51:02 -0700

    update namespace and add inputs to constructors

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Dealership
+namespace Bakery.Models
 {
   public class Program
   {
@@ -12,6 +12,13 @@ namespace Dealership
       Console.WriteLine("DEAl: Buy 2 get 1 free!");
       Console.WriteLine("PASTRY'S are $2 each.");
       Console.WriteLine("DEAL: 3 for $5!");
+      Console.WriteLine("How many loaves of bread would you like?");
+      int orderedBread = int.Parse(Console.ReadLine());
+      Console.WriteLine("How many pastries would you like?");
+      int orderedPastry = int.Parse(Console.ReadLine());
+
+      BreadOrder newBreadOrder = new BreadOrder(orderedBread);
+      PastryOrder newPastryOrder = new PastryOrder(orderedPastry);
     }
   }
 }
