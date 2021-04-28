126e4abaee7ea9708536e9f04ce5dc1cfa63c445
tlpackus <tlpackus@gmail.com>
2021-04-27 17:34:10 -0700

    add logic to display totals and add line breaks for clearity

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -12,13 +12,23 @@ namespace Bakery.Models
       Console.WriteLine("DEAl: Buy 2 get 1 free!");
       Console.WriteLine("PASTRY'S are $2 each.");
       Console.WriteLine("DEAL: 3 for $5!");
+      Console.WriteLine("-------------------------------------------");
+
       Console.WriteLine("How many loaves of bread would you like?");
       int orderedBread = int.Parse(Console.ReadLine());
+
       Console.WriteLine("How many pastries would you like?");
       int orderedPastry = int.Parse(Console.ReadLine());
 
       BreadOrder newBreadOrder = new BreadOrder(orderedBread);
       PastryOrder newPastryOrder = new PastryOrder(orderedPastry);
+      Console.WriteLine("-------------------------------------------");
+
+      Console.WriteLine($"You ordered {orderedBread} loaves of bread. Totaling ${newBreadOrder.BreadOrderPrice()}. ");
+
+      Console.WriteLine($"You ordered {orderedPastry} pastries. Totaling ${newPastryOrder.PastryOrderPrice()}. ");
+
+      Console.WriteLine($"GRAND TOTAL WITH SPECIAL OFFER APPLIED: ${newBreadOrder.BreadOrderPrice() + newPastryOrder.PastryOrderPrice()}");
     }
   }
 }
